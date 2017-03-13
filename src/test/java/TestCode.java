84fa6ab3303876c1be02010ca8034acb43cad1fd
a.paskhin <a.paskhin1@gmail.com>
2017-03-13 19:16:03 +0200

    kek

--- src/test/java/TestCode.java
+++ src/test/java/TestCode.java
@@ -1,31 +1,52 @@
 import io.github.bonigarcia.wdm.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by user on 09.03.2017.
  */
-public class TestCode {
+public class TestCode extends PageObjectMethods{
+
+    WebDriver.Options options;
     @BeforeClass
-//    @Test
     public void preparation() {
 //        String property = System.getProperty("user.dir") + "/drivers/chromedriver.exe";
 //        System.setProperty("webdriver.chrome.driver", property);
 
         ChromeDriverManager.getInstance().setup();
-        InternetExplorerDriverManager.getInstance().setup();
-        OperaDriverManager.getInstance().setup();
-        EdgeDriverManager.getInstance().setup();
-        PhantomJsDriverManager.getInstance().setup();
-        FirefoxDriverManager.getInstance().setup();ChromeDriverManager.getInstance().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://ekassa.com/#/");
+//        InternetExplorerDriverManager.getInstance().setup();
+//        OperaDriverManager.getInstance().forceCache().setup();
+//        EdgeDriverManager.getInstance().setup();
+//        PhantomJsDriverManager.getInstance().setup();
+//        FirefoxDriverManager.getInstance().setup();
+        ChromeDriverManager.getInstance().setup();
+        driver = new ChromeDriver();
+        options=driver.manage();
+        options.timeouts().implicitlyWait(7, TimeUnit.SECONDS);
+
     }
 
     @Test
     public void sampleTest() {
-        System.out.println("hello world. I love ekassa. Have a nice day :)");
+        goToMainPage();
+
+        submitRegForm();
     }
+
+//    @AfterClass
+//    public void teardown() {
+//        if (driver != null) {
+//            driver.quit();
+//        }
+//    }
 }
